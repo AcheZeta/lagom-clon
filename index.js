4c7d3cea741e6f6481198c90f70e085c42f99b59
AcheZeta <ache.mariana@gmail.com>
2020-05-22 19:09:43 -0500

    styles

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 const showHour = document.getElementById('hour');
-showHour.innerHTML = `<h1>JS Starter</h1>`;
+const showWeather = document.getElementById('weather');
+const showDay = document.getElementById('day');
+const showMonth = document.getElementById('day');
+
+
+const showtime = () => {
+    const hour = new Date().getHours()
+    const min = new Date().getMinutes()
+    const day = new Date().getDate()
+
+    showHour.innerHTML = `${hour}:${min}`;
+    showWeather.innerHTML = `Weather`;
+    showDay.innerHTML = `${day}`;
+}
+
+
+setInterval(showtime, 1000)
